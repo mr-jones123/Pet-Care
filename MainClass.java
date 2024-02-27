5cf6a23be23fa0ea92ffd9379a01d47b0b334ade
theXy <104372691+mr-jones123@users.noreply.github.com>
2024-02-28 02:14:42 +0800

    add: shops folder

--- MainClass.java
+++ MainClass.java
@@ -1,9 +1,12 @@
 import Interface_Super.*;
+import Shops.*;
 public class MainClass {
     public static void main(String[] args) throws Exception {
-        MenuDisplay menuDisplay = new MenuDisplay();
-        titleDisplay TitleDisplay = new titleDisplay();
-        TitleDisplay.TITLEDISPLAY();
-        menuDisplay.registrationDisplay();
+        initializeGraph GRAPH = new initializeGraph();
+        GRAPH.printGraph();
+        // MenuDisplay menuDisplay = new MenuDisplay();
+        // titleDisplay TitleDisplay = new titleDisplay();
+        // TitleDisplay.TITLEDISPLAY();
+        // menuDisplay.registrationDisplay();
     }
 }
